54db5503b5ba9f8c5d07632dfbd132307bd9e68f
Robson Garc <robsongeovane@hotmail.com>
2023-07-16 20:47:10 -0300

    Implemententado ListarClientes e SelecionarClientes

---
+++ br.com.projeto.dao/ClienteDAO.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Projeto_Controle_Vendas.br.com.projeto.model;
+using Projeto_Controle_Vendas.br.com.projeto.conexao;
+using System.Windows.Forms;
+using System.Data;
+
+namespace Projeto_Controle_Vendas.br.com.projeto.dao
+{
+    public class ClienteDAO
+    {
+        private MySqlConnection conexao;
+
+        public ClienteDAO() {
+            this.conexao = new ConnectionFactory().GetConnection();
+        }
+
+        #region CadastrarCliente
+        public void CadastrarCliente(Cliente obj)
+        {
+            try
+            {
+                //1 passo - definir o cmd - insert into
+                string sql = @"insert into tb_clientes (nome, rg, cpf, email, telefone, celular, cep, endereco, numero, complemento, bairro, cidade, estado)
+                               values (@nome, @rg, @cpf, @email, @telefone, @celular, @cep, @endereco, @numero, @complemento, @bairro, @cidade, @estado)";
+
+                //2 passo - Organizar o cmd sql
+                MySqlCommand executacmd = new MySqlCommand(sql, conexao);
+                executacmd.Parameters.AddWithValue("@nome", obj.nome);
+                executacmd.Parameters.AddWithValue("@rg", obj.rg);
+                executacmd.Parameters.AddWithValue("@cpf", obj.cpf);
+                executacmd.Parameters.AddWithValue("@email", obj.email);
+                executacmd.Parameters.AddWithValue("@telefone", obj.telefone);
+                executacmd.Parameters.AddWithValue("@celular", obj.celular);
+                executacmd.Parameters.AddWithValue("@cep", obj.cep);
+                executacmd.Parameters.AddWithValue("@endereco", obj.endereco);
+                executacmd.Parameters.AddWithValue("@numero", obj.numero);
+                executacmd.Parameters.AddWithValue("@complemento", obj.complemento);
+                executacmd.Parameters.AddWithValue("@bairro", obj.bairro);
+                executacmd.Parameters.AddWithValue("@cidade", obj.cidade);
+                executacmd.Parameters.AddWithValue("@estado", obj.estado);
+
+                //3 passo - Abrir a conexao e executar o cmd sql
+                conexao.Open();
+                executacmd.ExecuteNonQuery();
+
+                MessageBox.Show("Cliente cadastrado com sucesso!");
+            }
+
+            catch (Exception erro)
+            {
+
+                MessageBox.Show("Ocorreu o erro: " + erro);
+            }
+        }
+        #endregion
+
+        #region AlterarCliente
+        public void AlterarCliente(Cliente obj)
+        {
+            try
+            {
+                //1 passo - definir o cmd - insert into
+                string sql = @"update tb_clientes set nome=@nome,rg=@rg,cpf=@cpf,email=@email,telefone=@telefone,celular=@celular,cep=@cep, 
+                               endereco=@endereco,numero=@numero,complemento=@complemento,bairro=@bairro,cidade=@cidade,estado=@estado
+                               where id=id";
+
+                //2 passo - Organizar o cmd sql
+                MySqlCommand executacmd = new MySqlCommand(sql, conexao);
+                executacmd.Parameters.AddWithValue("@nome", obj.nome);
+                executacmd.Parameters.AddWithValue("@rg", obj.rg);
+                executacmd.Parameters.AddWithValue("@cpf", obj.cpf);
+                executacmd.Parameters.AddWithValue("@email", obj.email);
+                executacmd.Parameters.AddWithValue("@telefone", obj.telefone);
+                executacmd.Parameters.AddWithValue("@celular", obj.celular);
+                executacmd.Parameters.AddWithValue("@cep", obj.cep);
+                executacmd.Parameters.AddWithValue("@endereco", obj.endereco);
+                executacmd.Parameters.AddWithValue("@numero", obj.numero);
+                executacmd.Parameters.AddWithValue("@complemento", obj.complemento);
+                executacmd.Parameters.AddWithValue("@bairro", obj.bairro);
+                executacmd.Parameters.AddWithValue("@cidade", obj.cidade);
+                executacmd.Parameters.AddWithValue("@estado", obj.estado);
+                executacmd.Parameters.AddWithValue("@id", obj.codigo);
+
+                //3 passo - Abrir a conexao e executar o cmd sql
+                conexao.Open();
+                executacmd.ExecuteNonQuery();
+
+                MessageBox.Show("Cliente alterado com sucesso!");
+            }
+
+            catch (Exception erro)
+            {
+
+                MessageBox.Show("Ocorreu o erro: " + erro);
+            }
+        }
+
+        #endregion
+
+        #region ExcluirClientes
+
+        public void ExluirCliente(Cliente obj)
+        {
+            try
+            {
+                //1 passo - definir o cmd - insert into
+                string sql = @"delete from tb_clientes where id=@id";
+
+                //2 passo - Organizar o cmd sql
+                MySqlCommand executacmd = new MySqlCommand(sql, conexao);
+
+                executacmd.Parameters.AddWithValue("@id", obj.codigo);
+
+                //3 passo - Abrir a conexao e executar o cmd sql
+                conexao.Open();
+                executacmd.ExecuteNonQuery();
+
+                MessageBox.Show("Cliente excluido com sucesso!");
+            }
+
+            catch (Exception erro)
+            {
+
+                MessageBox.Show("Ocorreu o erro: " + erro);
+            }
+        }
+
+        #endregion
+
+        #region ListarClientes
+        public DataTable listarCliente()
+        {
+            try
+            {
+                //1 passo - Criar o DataTable e o comando sql
+                DataTable tabelacliente = new DataTable();
+
+                string sql = "select * from tb_clientes";
+
+                //2 passo - Organizar o comando sql e executar
+                MySqlCommand executacmd = new MySqlCommand(sql, conexao);
+                conexao.Open();
+                executacmd.ExecuteNonQuery();
+
+                //3 passo - Criar o MySQLDataApter para preencher os dados no DataTable;
+                MySqlDataAdapter da = new MySqlDataAdapter(executacmd);
+                da.Fill(tabelacliente);
+
+                return tabelacliente;
+            }
+            catch(Exception erro)
+            {
+                MessageBox.Show("Erro ao executar o comando sql: " + erro);
+                return null;
+            }
+        }
+        #endregion
+
+        //Metodo excluirCliente
+
+        //Metodo buscarClientePorCpf
+    }
+}
---
+++ br.com.projeto.view/Formclientes.cs
@@ -0,0 +1,92 @@
+﻿using Projeto_Controle_Vendas.br.com.projeto.dao;
+using Projeto_Controle_Vendas.br.com.projeto.model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Controle_Vendas.br.com.projeto.view
+{
+    public partial class Frmclientes : Form
+    {
+        public Frmclientes()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void btnnovo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnsalvar_Click(object sender, EventArgs e)
+        {
+            //1 passo - armazenar os dados em um objeto model
+            Cliente obj = new Cliente();
+
+            obj.nome = txtnome.Text; // obj.setName(txtnome.Text);
+            obj.rg = txtrg.Text;
+            obj.cpf = txtcpf.Text;
+            obj.email = txtemail.Text;
+            obj.telefone = txttelefone.Text;
+            obj.celular = txtcelular.Text;
+            obj.cep = txtcep.Text;
+            obj.endereco = txtendereco.Text;
+            obj.numero = int.Parse(txtnumero.Text);
+            obj.complemento = txtcomplemento.Text;
+            obj.bairro = txtbairro.Text;
+            obj.cidade = txtcidade.Text;
+            obj.estado = cbuf.Text;
+
+            //2 passo - Criar um obj da classe ClienteDAO e chamar o metodo CadastrarCliente
+            ClienteDAO dao = new ClienteDAO();
+            dao.CadastrarCliente(obj);
+        }
+
+        private void tabelaClientes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //Pegar os dados da linha selecionada 
+            txtcodigo.Text = tabelaClientes.CurrentRow.Cells[0].Value.ToString();
+            txtnome.Text = tabelaClientes.CurrentRow.Cells[1].Value.ToString();
+            txtrg.Text = tabelaClientes.CurrentRow.Cells[2].Value.ToString();
+            txtcpf.Text = tabelaClientes.CurrentRow.Cells[3].Value.ToString();
+            txtemail.Text = tabelaClientes.CurrentRow.Cells[4].Value.ToString();
+            txttelefone.Text = tabelaClientes.CurrentRow.Cells[5].Value.ToString();
+            txtcelular.Text = tabelaClientes.CurrentRow.Cells[6].Value.ToString();
+            txtcep.Text = tabelaClientes.CurrentRow.Cells[7].Value.ToString();
+            txtendereco.Text = tabelaClientes.CurrentRow.Cells[8].Value.ToString();
+            txtnumero.Text = tabelaClientes.CurrentRow.Cells[9].Value.ToString();
+            txtcomplemento.Text = tabelaClientes.CurrentRow.Cells[10].Value.ToString();
+            txtbairro.Text = tabelaClientes.CurrentRow.Cells[11].Value.ToString();
+            txtcidade.Text = tabelaClientes.CurrentRow.Cells[12].Value.ToString();
+            cbuf.Text = tabelaClientes.CurrentRow.Cells[13].Value.ToString();
+
+        }
+
+        private void Frmclientes_Load(object sender, EventArgs e)
+        {
+            ClienteDAO dao = new ClienteDAO();
+            tabelaClientes.DataSource = dao.listarCliente();
+        }
+    }
+}

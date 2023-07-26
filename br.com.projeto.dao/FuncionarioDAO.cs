a9c62ac25b2a0260f0cb10e38bbcd39229489c7b
Robson Garc <robsongeovane@hotmail.com>
2023-07-25 22:12:40 -0300

    Added Classe FuncionarioDAO e método CadastrarFuncionario

---
+++ br.com.projeto.dao/FuncionarioDAO.cs
@@ -0,0 +1,67 @@
+﻿using MySql.Data.MySqlClient;
+using Projeto_Controle_Vendas.br.com.projeto.conexao;
+using Projeto_Controle_Vendas.br.com.projeto.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Controle_Vendas.br.com.projeto.dao
+{
+    public class FuncionarioDAO
+    {
+        private MySqlConnection conexao;
+
+        public FuncionarioDAO()
+        {
+            this.conexao = new ConnectionFactory().GetConnection();
+        }
+
+        #region Cadastrar Funcionario
+
+        public void CadastrarFuncionario(Funcionario obj)
+        {
+            try
+            {
+                //1 passo - Criar o comando sql
+                string sql = @"insert into bdvendas.tb_funcionarios f (nome, rg, cpf, email, senha, cargo, nivel_acesso, telefone, celular, cep, endereco, numero, complemento, bairro, cidade, estado)
+                                value (@nome, @rg, @cpf, @email, @senha, @cargo, @nivel_acesso, @telefone, @celular, @cep, @endereco, @numero, @complemento, @bairro, @cidade, @estado);";
+                
+                //2 passo - Organizar e executar o comando sql
+                MySqlCommand executacmd = new MySqlCommand(sql, conexao);
+
+                executacmd.Parameters.AddWithValue("@nome", obj.nome);
+                executacmd.Parameters.AddWithValue("@rg", obj.rg);
+                executacmd.Parameters.AddWithValue("@cpf", obj.cpf);
+                executacmd.Parameters.AddWithValue("@email", obj.email);
+                executacmd.Parameters.AddWithValue("@senha", obj.senha);
+                executacmd.Parameters.AddWithValue("@cargo", obj.cargo);
+                executacmd.Parameters.AddWithValue("@nivel_acesso", obj.nivel_acesso);
+                executacmd.Parameters.AddWithValue("@telefone", obj.telefone);
+                executacmd.Parameters.AddWithValue("@celular", obj.celular);
+                executacmd.Parameters.AddWithValue("@cep", obj.cep);
+                executacmd.Parameters.AddWithValue("@endereco", obj.endereco);
+                executacmd.Parameters.AddWithValue("@numero", obj.numero);
+                executacmd.Parameters.AddWithValue("@complemento", obj.complemento);
+                executacmd.Parameters.AddWithValue("@bairro", obj.bairro);
+                executacmd.Parameters.AddWithValue("@cidade", obj.cidade);
+                executacmd.Parameters.AddWithValue("@estado", obj.estado);
+
+                //3 passo - Abrir a conexao e executar o cmd sql
+                conexao.Open();
+                executacmd.ExecuteNonQuery();
+
+                MessageBox.Show("Funcionário cadastrado com sucesso!");
+                conexao.Close();
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Ocorreu o erro" + erro);
+            }
+        }
+
+        #endregion
+    }
+}

df3075c1fdbf8c94ea8fbc142180a97f666f118d
Robson Garc <robsongeovane@hotmail.com>
2023-08-12 08:54:32 -0300

    Added Metodos Alterar e Excluir Funcionario

--- br.com.projeto.dao/FuncionarioDAO.cs
+++ br.com.projeto.dao/FuncionarioDAO.cs
@@ -55,6 +55,7 @@ namespace Projeto_Controle_Vendas.br.com.projeto.dao
                 executacmd.ExecuteNonQuery();
 
                 MessageBox.Show("Funcionário cadastrado com sucesso!");
+                //Fecha a conexao
                 conexao.Close();
             }
             catch (Exception erro)
@@ -96,6 +97,85 @@ namespace Projeto_Controle_Vendas.br.com.projeto.dao
             }
         }
 
+        #endregion
+
+        #region Alterar Funcionario
+
+        public void alterarFuncionario(Funcionario obj)
+        {
+            try
+            {
+                //1 passo - Criar o comando sql
+                string sql = @"update bdvendas.tb_funcionarios set nome=@nome, rg=@rg, cpf=@cpf, email=@email, senha=@senha, cargo=@cargo, nivel_acesso=@nivel_acesso, 
+                               telefone=@telefone, celular=@celular, cep=@cep, endereco=@endereco, numero=@numero, complemento=@complemento, bairro=@bairro, cidade=@cidade, estado=@estado 
+                               where id = @codigo" ;
+
+                //2 passo - Organizar e executar o comando sql
+                MySqlCommand executacmd = new MySqlCommand(sql, conexao);
+
+                executacmd.Parameters.AddWithValue("@nome", obj.nome);
+                executacmd.Parameters.AddWithValue("@rg", obj.rg);
+                executacmd.Parameters.AddWithValue("@cpf", obj.cpf);
+                executacmd.Parameters.AddWithValue("@email", obj.email);
+                executacmd.Parameters.AddWithValue("@senha", obj.senha);
+                executacmd.Parameters.AddWithValue("@cargo", obj.cargo);
+                executacmd.Parameters.AddWithValue("@nivel_acesso", obj.nivel_acesso);
+                executacmd.Parameters.AddWithValue("@telefone", obj.telefone);
+                executacmd.Parameters.AddWithValue("@celular", obj.celular);
+                executacmd.Parameters.AddWithValue("@cep", obj.cep);
+                executacmd.Parameters.AddWithValue("@endereco", obj.endereco);
+                executacmd.Parameters.AddWithValue("@numero", obj.numero);
+                executacmd.Parameters.AddWithValue("@complemento", obj.complemento);
+                executacmd.Parameters.AddWithValue("@bairro", obj.bairro);
+                executacmd.Parameters.AddWithValue("@cidade", obj.cidade);
+                executacmd.Parameters.AddWithValue("@estado", obj.estado);
+
+                executacmd.Parameters.AddWithValue("@codigo", obj.codigo);
+
+                //3 passo - Abrir a conexao e executar o cmd sql
+                conexao.Open();
+                executacmd.ExecuteNonQuery();
+
+                MessageBox.Show("Funcionário cadastrado com sucesso!");
+                //Fecha a conexao
+                conexao.Close();
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Aconteceu o erro: " + erro);
+            }
+        }
+
+
+        #endregion
+
+        #region Excluir Funcionario
+
+        public void deletarFuncionario(Funcionario obj)
+        {
+            try
+            {
+                //1 passo - Criar o comando sql
+                string sql = "delete from  bdvendas.tb_funcionarios where id = @codigo"; 
+
+                //2 passo - Organizar e executar o comando sql
+                MySqlCommand executacmd = new MySqlCommand(sql, conexao);
+
+                executacmd.Parameters.AddWithValue("@codigo", obj.codigo);
+
+                //3 passo - Abrir a conexao e executar o cmd sql
+                conexao.Open();
+                executacmd.ExecuteNonQuery();
+
+                MessageBox.Show("Funcionário excluído com sucesso!");
+                //Fecha a conexao
+                conexao.Close();
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Ocorreu o erro" + erro);
+            }
+        }
         #endregion
     }
 }
